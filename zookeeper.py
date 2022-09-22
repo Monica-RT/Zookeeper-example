701c8c85d2739a59198e0b5393c3bff351361c9a
Monica-RT <91165831+Monica-RT@users.noreply.github.com>
2022-09-21 23:37:14 -0500

    Update zookeeper.py
    Codigo con comentarios

--- zookeeper.py
+++ zookeeper.py
@@ -8,14 +8,17 @@ class Ztree:
     
     def get_tree(self):
         return self.tree
-
+#creacion de nodos. En caso de ser efimero, se asigna un tiempo de vida. En caso contrario, el valor de vida asignado sera None
     def create(self,path,data,ephemeral,OnService,timeDead,parent):
         
         if(ephemeral):
             self.tree.add_node(Node(path,path,data= Znode(path,data,ephemeral,OnService,timeDead)),parent= parent) 
         else:
             self.tree.add_node(Node(path,path,data= Znode(path,data,ephemeral,OnService,None)),parent= parent)
-
+#Funcion para eliminar nodos. Se corrobora la existencia del nodo solicitado
+#En caso de que la version del nodo no coincida, se genera un mensaje de error
+#En caso de que el nodo sea efimero, se imprime el tiempo de vida restante hasta que se elimina
+#En caso de que el nodo no este en servicio y sea efimero, se elimina (simulando la caida de un servidor)
     def delete(self,path,version):
         if(self.exist(path)):
             nodo = self.tree.get_node(path)
@@ -42,17 +45,18 @@ class Ztree:
                                 print("Tiempo restante de vida = {i}".format(i=i))
         else:
             print("Nodo no existe")
-            
+    #Busca un nodo a partir de su ruta, retornando True si existe y False si no        
     def exist(self,path):
         if(self.tree.contains(path)):
             return True
         else: 
             return False
-    
+    #Se imprime todo el arbol 
     def showTree(self):
         print("\t\t -------Estado actual del Arbol--------")
         self.tree.show()
-    
+    #Se verifica que nodo solicitado exista
+    #En caso afirmativo, se muestran el contenido del nodo
     def getData(self,path):
         if(self.exist(path)):
             nodo = self.tree.get_node(path)
@@ -60,7 +64,8 @@ class Ztree:
             return(nodoData.data)
         else:
             print("Nodo no existe")
-    
+    #Se verifica la existencia del nodo solicitado
+    #Se cambia el contenido del nodo por el ingresado 
     def setData(self,path,contenido):
         if(self.exist(path)):
             nodotemp = self.tree.get_node(path)
@@ -69,7 +74,8 @@ class Ztree:
             nodo.version = nodo.version + 1
         else:
             print("Nodo no existe")
-    
+    #Se verifica la existencia del nodo solicitado
+    #En caso afirmativo, se muestra el path del nodo, la version, el contenido y si es efimero o no
     def showNode(self,path):
         if(self.exist(path)):
             nodotemp = self.tree.get_node(path)
@@ -77,7 +83,8 @@ class Ztree:
             nodo.get_node_info()
         else:
             print("Nodo no existe")
-    
+    #Se verifica la existencia del nodo solicitado
+    #En caso afirmativo, se obtienen los hijos inmediatos del nodo solicitado
     def getChildren(self,path):
         if(self.exist(path)):
             nodotemp = self.tree.get_node(path)
@@ -90,7 +97,7 @@ class Ztree:
 
 
 
-
+#Definicion de la clase Znode con sus funciones y atributos
 class Znode:
     def __init__(self,path,data,ephemeral,OnService,timeDead):
         self.path = path
