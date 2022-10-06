2d70e4066842a150c2fc435a82fcecfe5cd4d082
Monica-RT <91165831+Monica-RT@users.noreply.github.com>
2022-10-05 19:08:12 -0500

    Update zookeeper_test.py

--- zookeeper_test.py
+++ zookeeper_test.py
@@ -9,16 +9,16 @@ class TestZookeeper(unittest.TestCase):
         tree.create('/nodo2','contenido2',True,True,20,'/nodo1')
         tree.create('/nodo3','contenido3',True,True,10,'/nodo1')
         tree.create('/nodo4','contenido4',False,True,0,'/nodo3')
-        tree.get_tree(self)
+        tree.showTree()
     def test_borrar_nodo(self):
         tree = Ztree()
         tree.create('/nodo1','contenido1',False,True,0,'/')
         tree.create('/nodo2','contenido2',True,True,20,'/nodo1')
         tree.create('/nodo3','contenido3',True,True,10,'/nodo1')
         tree.create('/nodo4','contenido4',False,True,0,'/nodo3')
-        tree.get_tree(self)
-        tree.delete('/nodo1/nodo3',0)
-        tree.get_tree(self)
+        tree.showTree()
+        tree.delete('//nodo1/nodo3',0)
+        tree.showTree()
 
     def test_obtener_hijos(self):
         tree = Ztree()
